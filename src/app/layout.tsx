0f135b8c681733f11253a9563ab221db3563f45c
Fernando Costa <costa.fernando@jovensgenios.com>
2025-04-20 10:10:26 -0300

    💄 Melhor funcionamento da Next fonts com Tailwind.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import type React from "react";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
 
 export const metadata = {
   title: "AI Grader",
@@ -18,7 +28,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html
+      lang="en"
+      className={`${inter.variable} ${roboto_mono.variable} antialiased`}
+    >
       <body>
         <ThemeProvider
           attribute="class"
