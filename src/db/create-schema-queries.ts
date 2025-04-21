3349dfcbe182c82dcf993dfba39026def0614b7e
Fernando Costa <costa.fernando@jovensgenios.com>
2025-04-21 15:56:30 -0300

    🔧 Atualização de queries para criação de schema do banco de dados.

--- src/db/create-schema-queries.ts
+++ src/db/create-schema-queries.ts
@@ -13,20 +13,20 @@ export const createSchemaQueries = [
     "answerSheetUrl" text NOT NULL,
     "score" numeric(3, 2),
     "feedback" text,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    "createdAt" timestamp DEFAULT now() NOT NULL,
+    "updatedAt" timestamp DEFAULT now() NOT NULL
   );`,
 
   `CREATE TABLE IF NOT EXISTS "exams" (
     "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "exams_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
     "name" text NOT NULL,
     "description" text,
-    "status" "exam_status" DEFAULT 'IN_PROGRESS',
+    "status" "exam_status" DEFAULT 'IN_PROGRESS' NOT NULL,
     "gradingRubric" text,
     "answerKey" text,
     "url" text,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    "createdAt" timestamp DEFAULT now() NOT NULL,
+    "updatedAt" timestamp DEFAULT now() NOT NULL
   );`,
 
   `DO $$
